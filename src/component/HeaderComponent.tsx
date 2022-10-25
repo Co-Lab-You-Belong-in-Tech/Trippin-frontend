7992b9c7ea8863b9751cffc102be84589dc6f453
shina1 <oswasiu@gmail.com>
2022-10-25 18:50:41 +0100

    created trip creat page

---
+++ src/component/HeaderComponent.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Box from "@mui/material/Box";
+import { Paper } from '@mui/material';
+import Container from '@mui/material/Container';
+import { Img } from './ImageComponent';
+import Logo from '../assets/final.png'
+const HeaderComponent = () => {
+  return (
+   <Paper elevation={4}>
+         <Box sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: "center",
+        minHeight: 90,
+    }}>
+            <Container maxWidth="xl">
+                    <Img alt={'triping'} src={Logo}/>
+            </Container>
+        </Box>
+   </Paper>
+  )
+}
+
+export default HeaderComponent
---
+++ src/component/TripPlanningForm.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button';
+import { TextInputField } from 'evergreen-ui'
+
+// icons
+
+// import {IoLocationSharp} from 'react-icons/io5'
+
+const TripPlanningForm = () => {
+  return (
+    <Box component="form" sx={{display: 'flex', flexDirection:'column', justifyContent:'center', alignItems: 'space-between', pl:2,pr:2}}>
+           <Box >
+                <TextInputField
+                id="tripname"
+                label="Name your trip"
+                inputHeight={40}
+                required
+                // description="This is a description."
+                placeholder="e.g. Trip to San Francisco"
+              />
+           </Box>
+           <Box>
+                <TextInputField
+                  id="location"
+                  label="Where to?"
+                  inputHeight={40}
+                  required
+                  // description="This is a description."
+                  placeholder="Add location"
+                />
+                {/* <IoLocationSharp  size={24}/> */}
+           </Box>
+           <Box sx={{display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
+            <Box >
+                <TextInputField
+                  id="start"
+                  label="Start date"
+                  type='date'
+                  width="270px"
+                  inputHeight={40}
+                  required
+                  // description="This is a description."
+                  placeholder="MM/DD/YY"
+                />
+            </Box>
+            <Box>
+                <TextInputField
+                  id="end"
+                  label="End date"
+                  type='date'
+                  required
+                  width="270px"
+                  inputHeight={40}
+                  // description="This is a description."
+                  placeholder="MM/DD/YY"
+                />
+           </Box>
+           </Box>
+           <Box>
+                <TextInputField
+                id="invite"
+                label="Invite your travel buddies"
+                inputHeight={40}
+                required
+                description="To invite more than one person please separete email accounts using a ;"
+                placeholder="johndoe@gmail.com; janedoe@gmail.com"
+              />
+           </Box>
+           <Box sx={{ '& button': { ml: 20 } }}>
+            <Button variant="contained" size="large">
+              Create New Itinerary
+            </Button>
+           </Box>
+    </Box>
+  )
+}
+
+export default TripPlanningForm
